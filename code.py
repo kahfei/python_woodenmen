5ea7bccee8d29b16dd45ef79ea51447d591a4fc9
kahfei <bak.kahfei@gmail.com>
2015-05-30 12:51:36 +0800

    complete secret code

--- code.py
+++ code.py
@@ -1,31 +1,31 @@
 import sys
+from itertools import groupby
 
 raw_data = sys.stdin.readlines()
 read_data = [ data.strip() for data in raw_data ]
 number_of_test_case = read_data[0]
 test_case = read_data[1:]
 
-
-
-
-def remove_identical_character(words):
-    iwords = iter(words)
-    clean_word = []
-    first_item = iwords.next()
-    second_item = iwords.next()
-    if first_item == second_item:
-      clean_word.append(first_item)
-    else:
-      clean_word.append(first_item)
-      clean_word.append(second_item)
-
-    return clean_word
-    
-    
-
-
 for case in test_case:
-  print remove_identical_character(case)
+  words = list(case)
+  uniq_words = "".join([ x[0] for x in groupby(words)])
+  print len(case) -  len(uniq_words)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
 
 
